4815bdc7c6889c61723d1d0410c836202d4fd23b
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-11-15 13:55:19 +0100

    fix: issue with PySpark / java ?

--- app/sandbox_ldi.py
+++ app/sandbox_ldi.py
@@ -1,33 +1,56 @@
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
 
 if __name__ == '__main__':
 
-    spark = SparkSession.builder.appName("test").getOrCreate()
+    # Create a Spark session
+    spark = SparkSession.builder \
+        .appName("DataFrameExample") \
+        .getOrCreate()
 
-    data = [
-            (
-                "0674", "Pont Haudaudine vers Sud", "657", "", "2", "0674 - Pont Haudaudine vers Sud", "2021-03-16",
-                "Hors Vacances"
-            ),
-            (
-                "0676", "Pont Willy Brandt vers Beaulieu", "480", "Faible", "1",
-                "0676 - Pont Willy Brandt vers Beaulieu", "2021-05-31", "Hors Vacances"
-            ),
-        ]
+    # Define the data
+    # data = [
+    #     (
+    #         "0674", "Pont Haudaudine vers Sud", "657", "Faible", "2",
+    #         "0674 - Pont Haudaudine vers Sud", "2021-03-16", "Hors Vacances"
+    #     ),
+    #     (
+    #         "0676", "Pont Willy Brandt vers Beaulieu", "480", "Faible", "1",
+    #         "0676 - Pont Willy Brandt vers Beaulieu", "2021-05-31", "Hors Vacances"
+    #     ),
+    # ]
+    #
+    # # Define the schema
+    # schema = StructType([
+    #     StructField("boucle_num", StringType(), True),
+    #     StructField("boucle_libelle", StringType(), True),
+    #     StructField("total", StringType(), True),
+    #     StructField("probabilite_presence_anomalie", StringType(), True),
+    #     StructField("jour_de_la_semaine", StringType(), True),
+    #     StructField("boucle_de_comptage", StringType(), True),  # Removed space
+    #     StructField("dateformat", StringType(), True),
+    #     StructField("vacances_zone_b", StringType(), True),
+    # ])
 
-    # Define the schema
-    schema = StructType([
-                StructField("boucle_num", StringType(), True),
-                StructField("boucle_libelle", StringType(), True),
-                StructField("total", StringType(), True),
-                StructField("probabilite_presence_anomalie", StringType(), True),
-                StructField("jour_de_la_semaine", StringType(), True),
-                StructField("Boucle de comptage", StringType(), True),
-                StructField("dateformat", StringType(), True),
-                StructField("vacances_zone_b", StringType(), True),
-        ])
 
-    df = spark.createDataFrame(data=data, schema=schema)
-    #df.show()
-    print("End")
+
+    data2 = [("James", "", "Smith", "36636", "M", 3000),
+             ("Michael", "Rose", "", "40288", "M", 4000),
+             ("Robert", "", "Williams", "42114", "M", 4000),
+             ("Maria", "Anne", "Jones", "39192", "F", 4000),
+             ("Jen", "Mary", "Brown", "", "F", -1)
+             ]
+
+    schema = StructType([StructField("firstname", StringType(), True), StructField("middlename", StringType(), True),
+                         StructField("lastname", StringType(), True), StructField("id", StringType(), True),
+                         StructField("gender", StringType(), True), StructField("salary", IntegerType(), True)])
+
+    # Create the DataFrame
+    df = spark.createDataFrame(data=data2, schema=schema)
+    #
+    # # Show the DataFrame
+    df.show()
+    #
+    # # Print schema to verify types
+    # df.printSchema()
+    print("end")
