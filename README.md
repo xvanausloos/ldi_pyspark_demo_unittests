5c2f553cc59966d36e7eaf31812fef8d26d95192
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-11-15 09:24:36 +0100

    feature: store df parquet files

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Created on 14/11/2024
 Resources:
 https://medium.com/plumbersofdatascience/introduction-to-creating-unit-tests-for-pyspark-applications-using-unittest-and-pytest-libraries-1fcc0feec945
 https://garybake.com/pyspark_pytest.html
+https://blog.eleven-labs.com/fr/demarrer-apache-spark/
 
 ## Prerequisite
 Java 11 or Java 8
@@ -21,3 +22,9 @@ From root folder:
 
 Run only tests with annotation `is_spark`
     
+## Read Parquet file 
+Using 
+```
+pip install parquet-cli
+parq <file name>.parquet --head 10
+``` 
--- app/pyspark_bike.py
+++ app/pyspark_bike.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.connect.client.core import logger
+from pyspark.sql.types import IntegerType, DateType
+
 from common_ldi.ldi_logger import LdiLogger
-from pathlib import Path, PosixPath
+from pathlib import Path
+from pyspark.sql import functions as F
 
 
 def main() -> None:
@@ -14,7 +18,10 @@ def main() -> None:
     # df = b.read_data_csv(csv_source_file)
     df = b.read_data_json(json_source_file)
     df.printSchema()
-    df.show(n=100)
+    df_clean = b.clean_data(df=df)
+    df_clean.show(n=100)
+    b.store_parquet(df_clean)
+    logger.info("** End of the script **")
 
 
 class Bike:
@@ -33,7 +40,23 @@ class Bike:
 
     def read_data_json(self, file: Path) -> DataFrame:
         df = self._spark.read.json(str(file))
+        df.printSchema()
+        df.show(n=10)
         return df
+
+    def clean_data(self, df: DataFrame) -> DataFrame:
+        df = (df.select(
+        F.col("boucle_num").alias("loop_number"),
+        F.col("boucle_libelle").alias("label"),
+        F.col("total").cast(IntegerType()).alias("total"),
+        F.col("dateformat").cast(DateType()).alias("date"),
+        F.col("vacances_zone_b").alias("holiday_name"),
+        )
+        .where(F.col("probabilite_presence_anomalie").isNull()))
+        return df
+
+    def store_parquet(self, df_clean: DataFrame) -> None:
+        df_clean.write.format("parquet").partitionBy("date").save("datalake/count-bike-nantes.parquet")
 
 
 if __name__ == "__main__":
